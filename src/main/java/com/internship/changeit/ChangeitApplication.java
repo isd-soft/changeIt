af045e77122a2111640e380683d32b6b15226589
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-09 12:45:59 +0200

    Initial commit

---
+++ src/main/java/com/internship/changeit/ChangeitApplication.java
@@ -0,0 +1,13 @@
+package com.internship.changeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangeitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internship/changeit/ChangeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.changeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
