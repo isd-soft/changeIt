365dca8721905d28a1e1ce842652d7f16f611078
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-10 11:19:23 +0200

    Added Specific Application Exceptions

---
+++ src/main/java/com/internship/changeit/exception/ApplicationException.java
@@ -0,0 +1,4 @@
+package com.internship.changeit.exception;
+
+public class ApplicationException extends RuntimeException{
+}
---
+++ src/main/java/com/internship/changeit/service/UserService.java
@@ -0,0 +1,19 @@
+package com.internship.changeit.service;
+
+import com.internship.changeit.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+
+    User getUserById(Long id);
+
+
+    User getUsersByEmail(String email);
+
+    void deleteUser(Long id);
+
+    boolean isEmailUnique(String email);
+}
