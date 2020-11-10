a13aa4f11149bb3cbdb7f9143ba01e90fe18947a
Catalin <ghermancatalin4@gmail.com>
2020-11-10 15:26:52 +0200

    added repository directory

---
+++ src/main/java/com/internship/changeit/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.internship.changeit.repository;
+
+import com.internship.changeit.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
