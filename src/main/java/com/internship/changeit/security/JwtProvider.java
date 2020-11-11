caecf09285601ad5a4837c2dfe6ea4023958fe50
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-11 12:15:08 +0200

    Externalize properties from JwtProvider to application.properies

--- src/main/java/com/internship/changeit/security/JwtProvider.java
+++ src/main/java/com/internship/changeit/security/JwtProvider.java
@@ -3,6 +3,7 @@ package com.internship.changeit.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,7 +11,9 @@ import java.util.Date;
 @Component
 public class JwtProvider {
 
+    @Value("${jwt.secret}")
     private String secretKey;
+    @Value("${jwt.expiration}")
     private Long validityInMilliseconds;
 
     public String createToken(String username, String role) {
@@ -18,13 +21,13 @@ public class JwtProvider {
         claims.put("role", role);
 
         Date now = new Date();
-        Date validity =  new Date(now.getTime() + 100500 * 1000);
+        Date validity =  new Date(now.getTime() + validityInMilliseconds * 1000);
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, "secretKey")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 }
