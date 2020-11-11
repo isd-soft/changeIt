e21bbdabdd6770863bb1b525527c4da826af2583
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-11 12:10:20 +0200

    Implemented generation JWT token

---
+++ src/main/java/com/internship/changeit/security/JwtProvider.java
@@ -0,0 +1,30 @@
+package com.internship.changeit.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private String secretKey;
+    private Long validityInMilliseconds;
+
+    public String createToken(String username, String role) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("role", role);
+
+        Date now = new Date();
+        Date validity =  new Date(now.getTime() + 100500 * 1000);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, "secretKey")
+                .compact();
+    }
+}
