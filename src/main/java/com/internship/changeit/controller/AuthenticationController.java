f76fbe9e4c66014a8e34a78f59e51ae2255f0807
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-11 14:38:55 +0200

    Config auth endpoint

--- src/main/java/com/internship/changeit/controller/AuthenticationController.java
+++ src/main/java/com/internship/changeit/controller/AuthenticationController.java
@@ -1,7 +1,19 @@
 package com.internship.changeit.controller;
 
+import com.internship.changeit.exception.ApplicationException;
+import com.internship.changeit.exception.ExceptionType;
+import com.internship.changeit.model.User;
+import com.internship.changeit.repository.UserRepository;
 import com.internship.changeit.security.AuthenticationRequestDto;
+import com.internship.changeit.security.JwtProvider;
+import com.internship.changeit.service.UserService;
+import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,14 +22,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtProvider jwtProvider;
+
     @PostMapping("/login")
     public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequestDto request){
-        return null;
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getEmail()));
+            User user = userService.getUserByEmail(request.getEmail());
+
+            if(user == null)
+                    throw  new ApplicationException(ExceptionType.USER_NOT_FOUND);
+
+            String token = jwtProvider.createToken(request.getEmail(), user.getRole().name());
+            Map<Object, Object> response = new HashMap<>();
+            response.put("email", request.getEmail());
+            response.put("token", token);
+
+            return ResponseEntity.ok(response);
+        }catch (AuthenticationException e) {
+            return new ResponseEntity<>("Invalid email/username combination", HttpStatus.FORBIDDEN);
+        }
     }
 
     @PostMapping("/logout")
