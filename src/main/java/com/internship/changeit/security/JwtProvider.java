05a0cc89c9f5e8da303b9470fc3ab748d4c5c0f4
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-11 13:29:39 +0200

    Add method to Validate token and extrat Username, also to add to toke in Authorization header, externlized authHeeader to application.properties

--- src/main/java/com/internship/changeit/security/JwtProvider.java
+++ src/main/java/com/internship/changeit/security/JwtProvider.java
@@ -1,21 +1,35 @@
 package com.internship.changeit.security;
 
 import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JwtProvider {
 
+    private final UserDetailsService userDetailsService;
+
     @Value("${jwt.secret}")
     private String secretKey;
     @Value("${jwt.expiration}")
     private Long validityInMilliseconds;
+    @Value("${header}")
+    private String authorizationHeader;
+
+    public JwtProvider(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @PostConstruct
     protected void init(){
@@ -44,5 +58,18 @@ public class JwtProvider {
         } catch (JwtException | IllegalArgumentException e) {
             throw  new JwtAuthenticationException("JWT token is expired or invalid", HttpStatus.FORBIDDEN);
         }
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request){
+        return request.getHeader(authorizationHeader);
     }
 }
