0527dfee1b7939235258dce3c0cea6453cafc526
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-17 21:33:40 +0200

    Removed INACTIVE problem status

--- src/main/resources/db/migration/V6__ins_into_problem.sql
+++ src/main/resources/db/migration/V6__ins_into_problem.sql
@@ -12,7 +12,7 @@
 	VALUES ('Rejected problems description', 'REJECTED', 'Rejected problem', 12, 1, 2, 4);
 	
 	INSERT INTO problem (description, status, title, votes, district_id, location_id, user_id) 
-	VALUES ('Inactive problems description', 'INACTIVE', 'Inactive problem', 0, 1, 1, 3);
+	VALUES ('Inactive problems description', 'ACTIVE', 'Inactive problem', 0, 1, 1, 3);
 	
 	INSERT INTO problem (description, status, title, votes, district_id, location_id, user_id) 
 	VALUES ('Another active problems description', 'ACTIVE', 'Another active problem', 120, 2, 4, 6);
