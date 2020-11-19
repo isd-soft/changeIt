9073e9e35350507106230bdb21e732a56019658b
schorsche94 <gcasandra94@gmail.com>
2020-11-19 16:19:30 +0200

    FIX:-save problem(return problem_id; add domain and add district id in db);

--- src/main/java/com/internship/changeit/dto/ProblemDto.java
+++ src/main/java/com/internship/changeit/dto/ProblemDto.java
@@ -1,8 +1,10 @@
 package com.internship.changeit.dto;
 
+import com.internship.changeit.controller.DomainController;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Data
@@ -15,6 +17,7 @@ public class ProblemDto {
     private Date created_at;
     private Date updated_at;
     private String status;
-    private LocationDto location;;
+    private LocationDto location;
+    private List<DomainDto> domains;
 
 }
