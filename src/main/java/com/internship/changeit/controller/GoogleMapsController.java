1cb4359f56c6f274c675ef2e61ede6ffa8974cd2
UrsuGheorghe <gursu@ms3-inc.com>
2020-11-22 22:25:08 +0200

    Added GoogleModelfor mappin from GoogleAPI response, implemented mappin proccess in GoogleMapController

--- src/main/java/com/internship/changeit/controller/GoogleMapsController.java
+++ src/main/java/com/internship/changeit/controller/GoogleMapsController.java
@@ -1,9 +1,12 @@
 package com.internship.changeit.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.internship.changeit.googleModels.GeocodeResult;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/geocode")
@@ -20,9 +24,9 @@ public class GoogleMapsController {
     private String apiKey;
 
     @GetMapping
-    public String getGeocode(@RequestParam String address) throws IOException {
+    public ResponseEntity<?> getGeocode(@RequestParam String address) throws IOException {
         OkHttpClient client = new OkHttpClient();
-        String encodedAddress = URLEncoder.encode(address, "UTF-8");
+        String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
         Request request = new Request.Builder()
                 .url("https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&address=" + encodedAddress)
                 .get()
@@ -30,6 +34,10 @@ public class GoogleMapsController {
                 .addHeader("x-rapidapi-key", apiKey)
                 .build();
         ResponseBody responseBody = client.newCall(request).execute().body();
-        return responseBody.string();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        GeocodeResult result = objectMapper.readValue(responseBody.string(), GeocodeResult.class);
+
+        return ResponseEntity.ok(result);
     }
 }
