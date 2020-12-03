74ec9898c92d733655bb0874bad5df960433b242
UrsuGheorghe <gursu@ms3-inc.com>
2020-12-03 16:24:44 +0200

    Change procfile

--- src/main/java/com/internship/changeit/config/AppConfigBean.java
+++ src/main/java/com/internship/changeit/config/AppConfigBean.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
